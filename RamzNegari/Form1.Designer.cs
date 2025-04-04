54d62cc423c028fa15bd84ec1a1863739862f311
Moein Noruzi <moein.nrz.dev@gmail.com>
2025-04-04 18:01:53 +0330

    lock File

---
+++ RamzNegari/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace RamzNegari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Btnlock = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BtnUnlock = new System.Windows.Forms.Button();
+            this.LabelPassword = new System.Windows.Forms.Label();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btnlock
+            // 
+            this.Btnlock.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            resources.ApplyResources(this.Btnlock, "Btnlock");
+            this.Btnlock.Name = "Btnlock";
+            this.Btnlock.UseVisualStyleBackColor = false;
+            this.Btnlock.Click += new System.EventHandler(this.Btnlock_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnUnlock
+            // 
+            this.BtnUnlock.BackColor = System.Drawing.Color.Lime;
+            resources.ApplyResources(this.BtnUnlock, "BtnUnlock");
+            this.BtnUnlock.Name = "BtnUnlock";
+            this.BtnUnlock.UseVisualStyleBackColor = false;
+            this.BtnUnlock.Click += new System.EventHandler(this.BtnUnlock_Click);
+            // 
+            // LabelPassword
+            // 
+            resources.ApplyResources(this.LabelPassword, "LabelPassword");
+            this.LabelPassword.Name = "LabelPassword";
+            this.LabelPassword.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // text1
+            // 
+            resources.ApplyResources(this.text1, "text1");
+            this.text1.Name = "text1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Cyan;
+            this.groupBox1.Controls.Add(this.LabelPassword);
+            this.groupBox1.Controls.Add(this.BtnUnlock);
+            this.groupBox1.Controls.Add(this.text1);
+            this.groupBox1.Controls.Add(this.label3);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Btnlock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnUnlock;
+        private System.Windows.Forms.Label LabelPassword;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Btnlock;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RamzNegari/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RamzNegari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnlock_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                try
+                {
+                    string userName = Environment.UserName;
+                    DirectorySecurity ds = Directory.GetAccessControl(path);
+                    FileSystemAccessRule accessRule = new FileSystemAccessRule(userName, FileSystemRights.FullControl, AccessControlType.Deny);
+                    ds.AddAccessRule(accessRule);
+                    Directory.SetAccessControl(path, ds);
+                    MessageBox.Show("پوشه قفل شد");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    throw;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK) ;
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                groupBox1.Visible = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //groupBox1.Visible = false;
+        }
+
+        private void BtnUnlock_Click(object sender, EventArgs e)
+        {
+            string Password = text1.Text;
+            if (Password == @"Passw@rd" && !string.IsNullOrEmpty(Password))
+            {
+                string path = folderBrowserDialog2.SelectedPath;
+                try
+                {
+                    string userName = Environment.UserName;
+                    DirectorySecurity ds = Directory.GetAccessControl(path);
+                    FileSystemAccessRule accessRule = new FileSystemAccessRule(userName, FileSystemRights.FullControl, AccessControlType.Deny);
+                    ds.RemoveAccessRule(accessRule);
+                    Directory.SetAccessControl(path, ds);
+                    MessageBox.Show("پوشه باز شد");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    throw;
+                }
+            }
+            else
+            {
+                MessageBox.Show("رمز اشتباه است");
+            }
+        }
+
+        private void checkedListBoxHide_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (checkedListBoxHide.Checked)
+            //{
+            //    text1.UseSystemPasswordChar=true;
+            //}
+            ////else
+            ////{
+            ////    text1.UseSystemPasswordChar = false;
+            ////}
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        //if (checkBoxX1.Checked)
+        //{
+        //    text1.UseSystemPasswordChar = false;
+        //}
+
+        //else 
+        //{
+        //    text1.UseSystemPasswordChar = true;
+        //}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
